089b2aad75419af27092474f6cd177f2901b2da5
Lee44 <jlee7772@yahoo.com>
2020-11-26 13:01:36 -0800

    Finished with inital setup of Django

--- Craigslist/Craigslist_Project/Craigslist_Project/asgi.py
+++ Craigslist/Craigslist_Project/Craigslist_Project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Craigslist project.
+ASGI config for Craigslist_Project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Craigslist.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Craigslist_Project.settings')
 
 application = get_asgi_application()
--- Craigslist/Craigslist_Project/Craigslist_Project/wsgi.py
+++ Craigslist/Craigslist_Project/Craigslist_Project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Craigslist project.
+WSGI config for Craigslist_Project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Craigslist.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Craigslist_Project.settings')
 
 application = get_wsgi_application()

79105873650d060a9e22601bab39116b06c96505
Lee44 <jlee7772@yahoo.com>
2020-11-29 23:46:29 -0800

    Working on making request to craigslist

--- Craigslist/Craigslist_Project/craigslist_app/models.py
+++ Craigslist/Craigslist_Project/craigslist_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+# Create your models here.
+class Search(models.Model):
+    search = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
+
+    # In admin site, when a search is created, it will show the string that was searched
+    def __str__(self):
+        return '{}'.format(self.search)
+
+    # In admin site, the database column is named Searchs by Django but the correct spelling is Searches
+    # class Meta makes the spelling correction
+    class Meta:
+        verbose_name_plural = 'Searches'
--- Craigslist/Craigslist_Project/craigslist_app/views.py
+++ Craigslist/Craigslist_Project/craigslist_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 
 # Create your views here.
+def home(request):
+    return render(request,"base.html")
+
+def new_search(request):
+    # search
+    search = request.POST.get('search')
+    response = requests.get(BASE_CRAIGSLIST_URL)
+    data = response.text
+
+    stuff_for_frontend = {
+        'search': search,
+    }
+
+    return render(request,'craigslist_app/new_search.html',stuff_for_frontend)

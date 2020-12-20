fc6481f9852a5dc1960950f73815a8bf0e15539b
Lee44 <jlee7772@yahoo.com>
2020-12-19 18:40:41 -0800

    Deleted Ecommerce and restarted on Mac. It works

--- Ecommerce/ecommerce/store/urls.py
+++ Ecommerce/ecommerce/store/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Leave as empty string for base url
-    path('', views.store, name="store"),
-    path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout"),
+	#Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
 
 ]
--- Ecommerce/ecommerce/store/views.py
+++ Ecommerce/ecommerce/store/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
 
-
 def store(request):
-    context = {}
-    return render(request, 'store/store.html', context)
-
+	context = {}
+	return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
-    return render(request, 'store/cart.html', context)
-
+	context = {}
+	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
-    return render(request, 'store/checkout.html', context)
+	context = {}
+	return render(request, 'store/checkout.html', context)
